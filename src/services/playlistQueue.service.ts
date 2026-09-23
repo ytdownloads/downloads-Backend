948d943769586c8e66519d1ae6677c5c7607ca9d
TranscriptAi <kiritokun5796@gmail.com>
2026-09-23 14:25:38 +0530

    fix: use tv_embedded,android_vr,mweb,web player client for downloads

--- src/services/playlistQueue.service.ts
+++ src/services/playlistQueue.service.ts
@@ -107,7 +107,7 @@ export class PlaylistQueueService {
         '--windows-filenames',
         '--no-warnings',
         '--extractor-args',
-        'youtube:player_client=mweb,web',
+        'youtube:player_client=tv_embedded,android_vr,mweb,web',
         '--no-mtime',
         '--buffer-size',
         '1024k',
